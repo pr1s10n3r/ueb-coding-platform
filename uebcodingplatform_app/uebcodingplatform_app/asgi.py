a945db6e00043c3245a6eba24b8cc687e919c93f
pr1s10n3r <alvarostagg@protonmail.com>
2022-10-15 13:07:47 -0500

    ux: initial dashboard template view

--- uebcodingplatform_app/uebcodingplatform_app/asgi.py
+++ uebcodingplatform_app/uebcodingplatform_app/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uebcodingplatform_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'uebcodingplatform_app.settings')
 
 application = get_asgi_application()
