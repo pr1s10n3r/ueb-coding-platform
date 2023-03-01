9101b62c1db359c857af9842e68f5cb8124ac84b
pr1s10n3r <alvarostagg@protonmail.com>
2023-02-28 21:38:29 -0500

    style: initial UI

---
+++ frontend/src/lib/LoaderPage.svelte
@@ -0,0 +1,12 @@
+<div>
+  <h3 class="page-title">Primero, debes subir algunos archivos.</h3>
+  <p>
+    Asegurate de subir unicamente archivos sueltos con extension .java. Para una
+    mejor visualización de los resultados, te sugerimos ponerles nombres
+    signifivativos como por ejemplo el correo (sin el dominio) del estudiante
+    que desarrolló el código fuente.
+  </p>
+</div>
+
+<style>
+</style>
