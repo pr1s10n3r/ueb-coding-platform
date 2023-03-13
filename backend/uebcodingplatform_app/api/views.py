0ece4ab1c9ec9a777f17ded4d570ab263666009a
KamikazeeM <rgarciamo@unbosque.edu.co>
2023-03-13 02:19:55 +0000

    feat. resolvers implemented.

--- backend/uebcodingplatform_app/api/views.py
+++ backend/uebcodingplatform_app/api/views.py
@@ -2,26 +2,38 @@ from rest_framework import status, viewsets
 from rest_framework.response import Response
 from api.serializers import DummySerializer
 from django.core.files.storage import FileSystemStorage
-import zipfile, os, docker, os.path, shutil
-
+from api.resolvers.complexity import Complexity
+from api.resolvers.organizer import Organizer
+from api.resolvers.script import Script
+from api.resolvers.docker import Docker
+from api.resolvers.measurer import Measurer
+from api.resolvers.cleaner import Cleaner
 
 class DummyViewSet(viewsets.ViewSet):
     def list(self, req):
         return Response('Ueb')
 
     def create(self, req):
+        complexity = Complexity()
+        organizer = Organizer()
+        script = Script()
+        docker = Docker()
+        measurer = Measurer()
+        cleaner = Cleaner()
+
         serializer = DummySerializer(data=req.data)
         if serializer.is_valid():
-            self.updload_file(serializer.validated_data['file'])
-            self.create_docker()
-            return Response({'status': 'funciona'})
+            data_dict = serializer.validated_data
+            program_input = data_dict["input"]
+            program_function = data_dict["function"]
+            program_dir, filename = organizer.resolve(data_dict["file"])
+            full_path = program_dir + "/" + filename
+            complexity_comp = complexity.resolve(full_path, program_function)
+            script.resolve(program_dir, program_input)
+            docker.resolve("11", program_input, program_dir, filename, script.bash_filename)
+            measurer.resolve(program_dir, script.env_filename)
+            cleaner.resolve(program_dir)
+            return Response({'status': 'it works'})
         else:
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
-
-    def upload_file(self, f):
-        FileSystemStorage(location="/tmp").save('main.java', f)
-
-    def create_docker(self):
-        client = docker.from_env()
-        client.containers.run('openjdk:11', 'printf \'3\n3\n1\n2\n\' | java main.java', volumes=['/tmp:/mnt/vol1'], working_dir='/mnt/vol1')
