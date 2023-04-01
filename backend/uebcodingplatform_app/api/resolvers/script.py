1f642597ab32178cff36861e9376d20d3e7118b6
Álvaro Stagg <5404862+pr1s10n3r@users.noreply.github.com>
2023-03-31 22:42:15 -0500

    Merge pull request #4 from pr1s10n3r/feature/docker_fix
    merge docker fix to master

--- backend/uebcodingplatform_app/api/resolvers/script.py
+++ backend/uebcodingplatform_app/api/resolvers/script.py
@@ -1,25 +1,22 @@
+import os, shutil
+
 class Script:
 
     new_line = "\n"
+    script_dirname = "scripts"
     env_filename = "env.txt"
     bash_filename = "bash.sh"
 
     def resolve(self, program_dir, input):
-        with open("%s/%s" % (program_dir, self.env_filename), "w") as env_file:
-            env_file.write("TIME = 0")
+        path_str = "%s/%s"
+        script_dir = path_str % (os.path.dirname(os.path.dirname(__file__)), self.script_dirname)
+        
+        # env
+        env_srcfile_path = path_str  % (script_dir, self.env_filename)
+        env_dstfile_path = path_str % (program_dir, self.env_filename)
+        shutil.copy2(env_srcfile_path, env_dstfile_path)
 
-        with open("%s/%s" % (program_dir, self.bash_filename), "w") as bash_file:
-            lines = []
-            lines.append("#!/bin/bash")
-            lines.append(self.new_line)
-            lines.append("start=$(date -u +%s%N)")
-            lines.append(self.new_line)
-            lines.append("printf $1 | java $2")
-            lines.append(self.new_line)
-            lines.append("end=$(date -u +%s%N)")
-            lines.append(self.new_line)
-            lines.append("TIME=\"$((($end-$start)/1000000))\"")
-            lines.append(self.new_line)
-            lines.append("sed -i -r \"s/^(TIME =).*/\\1 $TIME/\" env.txt")
-            bash_file.writelines(lines)
-
+        # bash
+        bash_srcfile_path = path_str % (script_dir, self.bash_filename)
+        bash_dstfile_path = path_str % (program_dir, self.bash_filename)
+        shutil.copy2(bash_srcfile_path, bash_dstfile_path)
--- backend/uebcodingplatform_app/api/serializers.py
+++ backend/uebcodingplatform_app/api/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 
 class DummySerializer(serializers.Serializer):
-    file = serializers.FileField()
-    time = serializers.FloatField()
-    input = serializers.CharField(max_length=200)
     complexity = serializers.ChoiceField(
             choices=[
                 'constant', 
-                'logarithmic', 
                 'linear',
-                'Nlogarithmic',
+                'linearithmic',
                 'quadratic',
-                'cubic',
-                'polynomial'
+                'cubic'
             ])
+    file = serializers.FileField()
     function = serializers.CharField(max_length=200)
+    input = serializers.CharField(max_length=200)
+    output = serializers.CharField(max_length=200)
+    time = serializers.FloatField()
+
