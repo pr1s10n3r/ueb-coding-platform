899eeb7036a9f2e548915025105777dedbaf8806
KamikazeeM <rgarciamo@unbosque.edu.co>
2023-04-14 01:51:10 +0000

    fix. bash script.

--- backend/uebcodingplatform_app/api/scripts/bash.sh
+++ backend/uebcodingplatform_app/api/scripts/bash.sh
@@ -9,10 +9,9 @@ compiler_result=$?
 if [ $compiler_result -eq 0 ]
 then
   output=$(echo $1 | java $2)
+  end=$(date -u +%s%N)
+  time=$((($end-$start)/1000000))
+  sed -i -r "s/^(TIME=).*/\1$(echo $time)/" env.txt
+  sed -i -r "s/^(OUTPUT=).*/\1$(echo $output)/" env.txt
+  sed -i -r "s/^(COMPILER_RESULT=).*/\1$(echo $compiler_result)/" env.txt
 fi
-
-end=$(date -u +%s%N)
-time=$((($end-$start)/1000000))
-sed -i -r "s/^(TIME=).*/\1$(echo $time)/" env.txt
-sed -i -r "s/^(OUTPUT=).*/\1$(echo $output)/" env.txt
-sed -i -r "s/^(COMPILER_RESULT=).*/\1$(echo $compiler_result)/" env.txt
